0e70fe7273c21745b75d761514853c2b3d47df27
aram2726 <aramharoyan@gmail.com>
2019-12-16 17:21:36 +0400

    add README

---
+++ README.md
@@ -0,0 +1,10 @@
+# Polls App
+--------------
+
+#### simple app to test [package multiple-permissions](https://pypi.org/project/multiple-permissions/)
+
+* #### usage
+
+    * make virtualenv (the way you would like)
+    * isntall requirements from **requirements.txt**
+    * and run app
--- apps/polls/tests.py
+++ apps/polls/tests.py
@@ -1,3 +1,42 @@
-from django.test import TestCase
+import pytest
 
-# Create your tests here.
+from django.contrib.auth.models import AnonymousUser, User
+from django.test import RequestFactory
+
+from multiple_permissions.middlewares import PermissionMiddleware
+
+from .views import PollsListView, PollsCreateView
+
+pytestmark = pytest.mark.django_db
+
+
+class SimpleTest:
+    def __init__(self):
+        # Every test needs access to the request factory.
+        self.factory = RequestFactory()
+        self.user = User.objects.create_user(
+            username='jacob', email='jacob@…', password='top_secret', is_superuser=True, is_staff=True)
+
+    @pytestmark
+    def test_details(self):
+        # Create an instance of a GET request.
+        request = self.factory.get('/polls')
+
+        # Recall that middleware are not supported. You can simulate a
+        # logged-in user by setting request.user manually.
+        request.user = self.user
+        PermissionMiddleware(object).process_view(request, PollsListView, [], {})
+        response = PollsListView.as_view()(request)
+        assert response.status_code == 200
+
+    @pytestmark
+    @pytest.mark.xfail
+    def test_to_fail(self):
+        request = object
+        # an AnonymousUser instance.
+        request.user = AnonymousUser()
+
+        # Use this syntax for class-based views.
+        response = PollsCreateView.as_view()(request)
+        PermissionMiddleware(object).process_view(request, PollsCreateView, [], {})
+        assert response.status_code == 200
